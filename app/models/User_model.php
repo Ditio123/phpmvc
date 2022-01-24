367eb97b7187478923b0b6e320a8c9cc89f9b3fc
Ditio123 <93955981+Ditio123@users.noreply.github.com>
2022-01-24 11:21:09 +0700

    Add files via upload

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Rizal Ditio';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>" alt="Rizal Ditio" width="120" class="rounded-circle shadow">
+    <p>Halo, Nama saya adalah <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun, Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
